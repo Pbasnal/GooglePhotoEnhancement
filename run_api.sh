d70b9d169ba18b0b3ec7bd3f2c965be2251b63cf
Pankaj Basnal <pankaj.basnal@swiggy.in>
2021-11-28 13:09:21 +0530

    Writing the creds to file

--- run_api.sh
+++ run_api.sh
@@ -4,7 +4,4 @@ export LANG=en_US.utf-8
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-echo "Google credentials"
-echo $GOOGLE_CLIENT_CREDENTIAL
-echo "---"
 flask run --host=0.0.0.0 --port=8000
