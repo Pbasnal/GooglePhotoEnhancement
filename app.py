8df61974d1f84c1f9ebc21327db04a9f66624428
Pankaj Basnal <pankaj.basnal@swiggy.in>
2022-03-25 21:56:45 +0530

    working code

--- app.py
+++ app.py
@@ -1,44 +1,63 @@
-import os, pika, threading
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from flask_restful import Api
+import os
+import threading
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['LC_ALL'] = 'en_US.utf-8'
+os.environ['LANG'] = 'en_US.utf-8'
+os.environ['FLASK_APP'] = 'app.py'
+os.environ['FLASK_ENV'] = 'development'   
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
+
+from flask_restful import Api
+
 api = Api(app)
+def addApis():
+    from apis.HelloWorld import HelloWorld, SimilarImages
+    api.add_resource(HelloWorld, '/')
+    api.add_resource(SimilarImages, '/similarimages')
+    
+    from apis.AuthorizeWithGoogle import AuthorizeWithGoogle
+    api.add_resource(AuthorizeWithGoogle, '/auth')
 
-from apis.PhotoAlbums import PhotoAlbums
-from apis.HelloWorld import HelloWorld
-from apis.AuthorizeWithGoogle import AuthorizeWithGoogle
+    from apis.PhotoAlbums import PhotoAlbums
+    api.add_resource(PhotoAlbums(api), '/albums')
 
-api.add_resource(AuthorizeWithGoogle, '/auth')
-api.add_resource(PhotoAlbums, '/albums')
-api.add_resource(HelloWorld, '/')
+addApis()
 
 def startWorkers():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-
-    from apis.messagebus import Bus
-    Bus.registerQueues(channel)
-
+    print("Starting workers")
+    from apis.messagebus import MessageBus
+    Bus = MessageBus()
     import apis.userBackgroundProcess as userListener
     import apis.photoBackgroundProcess as photoListener
-    userListener.registerMessageListener(channel)
-    photoListener.registerMessageListener(channel)
-    
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+
+    Bus.addListener(photoListener)
+    Bus.addListener(userListener)
+
+    Bus.startListeners("localhost")
+
+def startServer(debug=False):
+
+    print(f"API routes \n{app.url_map}")
+    app.run(
+        # host='localhost',
+        port=8080,
+        debug=debug,
+        ssl_context="adhoc")
+    # app.run(debug=debug, ssl_context="adhoc")
+    # app.run()
 
 workerThread = threading.Thread(target=startWorkers)
 workerThread.start()
 
 if __name__ == '__main__':
-    # app.run('localhost', 8080, debug=True, ssl_context="adhoc")
-    app.run(debug=True, ssl_context="adhoc")
-    #app.run()
+
+    startServer()
--- run_api.sh
+++ run_api.sh
@@ -1,8 +1,9 @@
+export OAUTHLIB_INSECURE_TRANSPORT=1
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-# docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
-flask run --port=8080 #--host=0.0.0.0
+docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
+flask run --port=8080 --host=0.0.0.0
