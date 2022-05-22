14211b9c0a28b0f3381a671190fd05d943485277
Pbasnal <pankajbasnal17@gmail.com>
2022-05-22 11:41:22 +0530

    logging google creds

--- run_api.sh
+++ run_api.sh
@@ -3,4 +3,8 @@ export LANG=en_US.utf-8
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
+
+echo "Google credentials"
+echo $GOOGLE_CLIENT_CREDENTIAL
+echo "---"
 flask run --host=0.0.0.0 --port=8000
